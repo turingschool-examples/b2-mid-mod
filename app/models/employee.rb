8fd9ee12917b89ac37eb99c9106f6e4f97e88cca
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-02-18 14:05:59 -0700

    feat: add org_tickets instance method on Employees Class

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,4 +2,8 @@ class Employee < ApplicationRecord
   belongs_to :department
   has_many :employee_tickets
   has_many :tickets, through: :employee_tickets
+
+  def org_tickets
+    tickets.order(age: :desc)
+  end
 end
